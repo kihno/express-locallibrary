c0fa383917d3f33e7b922bd07b76f392c07d90ef
kihno <stistyle13@gmail.com>
2022-09-09 14:56:27 -0700

    Create book instance view and controller render

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -38,8 +38,33 @@ exports.book_list = (req, res, next) => {
         });
 };
 
-exports.book_detail = (req, res) => {
-    res.send(`NOT IMPLEMENTED: book detail: ${req.params.id}`);
+exports.book_detail = (req, res, next) => {
+    async.parallel(
+        {
+            book(callback) {
+                Book.findById(req.params.id)
+                .populate('author')
+                .populate('genre')
+                .exec(callback)
+            },
+            book_instance(callback) {
+                BookInstance.find({ book: req.params.id }).exec(callback);
+            },
+        },
+        (err, results) => {
+            if (err) {
+                return next(err);
+            }
+
+            if (results == null) {
+                const err = new Error('Book not found');
+                err.status = 404;
+                return next(err);
+            }
+
+            res.render('book_detail', { title: results.book.title, book: results.book, book_instances: results.book_instance});
+        }
+    );
 };
 
 exports.book_create_get = (req, res) => {
