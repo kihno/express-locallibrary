3ca4837224c96d4f3ae72782397de98d574fa027
kihno <stistyle13@gmail.com>
2022-09-13 11:34:21 -0700

    Clean up code

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const app = express();
 //MongoDB
 const mongoose = require('mongoose');
 dotenv.config();
-const mongoDB = process.env.MONGODB_URI;
+const dev_db_url = 'mongodb+srv://director:abraxas@cluster0.rlinavw.mongodb.net/?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
